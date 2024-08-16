b7a7aaac09e8f1d830a1e49de1f777dc100d51c1
electricbayan <a.volkovich@internet.ru>
2024-08-16 23:33:30 +0500

    Added /tell, /prank updated

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from aiogram import Router
 from aiogram.filters.command import Command
 from aiogram.types import Message, BotCommand
 from aiogram import Bot, Dispatcher
-from aiogram.filters import StateFilter
+from aiogram.filters import StateFilter, CommandObject
 from aiogram.fsm.context import FSMContext
 
 from src.states import PrankState
@@ -40,35 +40,66 @@ async def get_players(message: Message):
 
 
 @dp.message(Command('prank'))
-async def prank_player_getnick(message: Message, state: FSMContext):
-    await message.answer('Введите ник')
-    await state.set_state(PrankState.nick)
-
-@dp.message(PrankState.nick)
-async def prank_player(message: Message, state: FSMContext):
-    async with Client(settings.SERVER_IP, settings.SERVER_PORT, settings.SERVER_PASSWORD) as client:
-        resp = await client.send_cmd('/list')
-    msg = message.text.strip()
-    if msg in resp[0]:
+async def prank_player_getnick(message: Message, command: CommandObject):
+    try:
+        args = command.args.split(" ")
+    except AttributeError:
+        await message.answer('Синтаксис команды: /prank <playername> <text (optional)>')
+    if len(args) == 1:
         async with Client(settings.SERVER_IP, settings.SERVER_PORT, settings.SERVER_PASSWORD) as client:
-            resp = await client.send_cmd(f'/title {msg} times 0t 70t 20t')
-            await client.send_cmd(f'/title {msg} title "пидор"')
-        print(resp)
-        await message.answer("Успешно")
-    else:
-        await message.answer('Такого игрока не существует')
-        print(msg)
-        print(resp)
+            resp = await client.send_cmd('/list')
+        msg = args[0]
+        if msg in resp[0]:
+            async with Client(settings.SERVER_IP, settings.SERVER_PORT, settings.SERVER_PASSWORD) as client:
+                resp = await client.send_cmd(f'/title {msg} times 0t 70t 20t')
+                await client.send_cmd(f'/title {msg} title "пидор"')
+            print(resp)
+            await message.answer("Успешно")
+        else:
+            await message.answer('Такого игрока не существует')
+            print(msg)
+            print(resp)
 
-    await state.clear()
+    else:
+        async with Client(settings.SERVER_IP, settings.SERVER_PORT, settings.SERVER_PASSWORD) as client:
+            resp = await client.send_cmd('/list')
+        msg = args[0]
+        text = ' '.join(args[1:])
+        if msg in resp[0]:
+            async with Client(settings.SERVER_IP, settings.SERVER_PORT, settings.SERVER_PASSWORD) as client:
+                resp = await client.send_cmd(f'/title {msg} times 0t 70t 20t')
+                await client.send_cmd(f'/title {msg} title "{text}"')
+            print(resp)
+            await message.answer("Успешно")
+        else:
+            await message.answer('Такого игрока не существует')
+            print(msg)
+            print(resp)
+
+@dp.message(Command('tell'))
+async def send_message(message: Message, command: CommandObject):
+    try:
+        args = command.args.split(" ")
+    except AttributeError:
+        await message.answer('Синтаксис команды: /tell <text>')
+    if len(args) == 1:
+        async with Client(settings.SERVER_IP, settings.SERVER_PORT, settings.SERVER_PASSWORD) as client:
+            # print(args)
+            await client.send_cmd(f'/say {" ".join(args)}')
+
+    else:
+        pass
 
     
 
 
 async def main():
     load_dotenv()
-    commands = [BotCommand(command="/players", description="Список игроков")]
+    commands = [BotCommand(command="/players", description="Список игроков"),
+                BotCommand(command="/tell", description="Написать в чат"),
+                BotCommand(command='/prank', description="Пранкануть чувачка")]
     bot = Bot(settings.BOT_TOKEN)
+    bot.set_my_commands(commands)
     await dp.start_polling(bot)
 
 
