7ec5b01f08b11ed197b5e106378c35a6936e3b9b
electricbayan <a.volkovich@internet.ru>
2024-08-17 11:54:19 +0500

    Improved architecture

--- src/routers/main_rt.py
+++ src/routers/main_rt.py
@@ -3,6 +3,7 @@ from aiogram.filters.command import Command
 from aiogram.types import Message
 from config import settings
 from aiomcrcon import Client
+from aiogram.filters import CommandObject
 
 
 main_rt: Router = Router()
@@ -18,8 +19,64 @@ async def get_players(message: Message):
     players_count = int(resp[2])
     if players_count == 1:
         players_noun = 'игрок'
-    elif 2 <= players_count <= 4:
+    elif 1 < players_count < 5:
         players_noun = 'игрока'
 
     players = resp[10:]
-    await message.answer(f'Сейчас на сервере: {players_count} {players_noun}.')
+    response = f'Сейчас на сервере: {players_count} {players_noun}.\n'
+    for player in players:
+        response += ':- ' + str(player) + '\n'
+    await message.answer(response)
+
+
+@main_rt.message(Command('prank'))
+async def prank_player_getnick(message: Message, command: CommandObject):
+    try:
+        args = command.args.split(" ")
+    except AttributeError:
+        await message.answer('Синтаксис команды: /prank <playername> <text (optional)>')
+    if len(args) == 1:
+        async with Client(settings.SERVER_IP, settings.SERVER_PORT, settings.SERVER_PASSWORD) as client:
+            resp = await client.send_cmd('/list')
+        msg = args[0]
+        if msg in resp[0]:
+            async with Client(settings.SERVER_IP, settings.SERVER_PORT, settings.SERVER_PASSWORD) as client:
+                resp = await client.send_cmd(f'/title {msg} times 0t 70t 20t')
+                await client.send_cmd(f'/title {msg} title "пидор"')
+            print(resp)
+            await message.answer("Успешно")
+        else:
+            await message.answer('Такого игрока не существует')
+            print(msg)
+            print(resp)
+
+    else:
+        async with Client(settings.SERVER_IP, settings.SERVER_PORT, settings.SERVER_PASSWORD) as client:
+            resp = await client.send_cmd('/list')
+        msg = args[0]
+        text = ' '.join(args[1:])
+        if msg in resp[0]:
+            async with Client(settings.SERVER_IP, settings.SERVER_PORT, settings.SERVER_PASSWORD) as client:
+                resp = await client.send_cmd(f'/title {msg} times 0t 70t 20t')
+                await client.send_cmd(f'/title {msg} title "{text}"')
+            print(resp)
+            await message.answer("Успешно")
+        else:
+            await message.answer('Такого игрока не существует')
+            print(msg)
+            print(resp)
+
+@main_rt.message(Command('tell'))
+async def send_message(message: Message, command: CommandObject):
+    try:
+        args = command.args.split(" ")
+        if len(args):
+            async with Client(settings.SERVER_IP, settings.SERVER_PORT, settings.SERVER_PASSWORD) as client:
+                # print(args)
+                text = " ".join(args) + "\n\n@" + message.from_user.first_name
+                await client.send_cmd(f'/say {text}')
+
+        else:
+            pass
+    except AttributeError:
+        await message.answer('Синтаксис команды: /tell <text>')
--- src/states.py
+++
@@ -1,6 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class PrankState(StatesGroup):
-    nick = State()
-    
